12a4f43906199cd42c8ea8f08e084237c655e63f
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2022-03-05 02:02:38 -0800

    Minnor lints

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,11 +3,11 @@ import Header from './Header';
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <>
         <Header />
         {children}
-    </div>
+    </>
   )
 }
 
-export default Layout
+export default Layout;
